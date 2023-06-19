92f2d6f97b707ae424da124c0d97a3ed336a23ab
jamshaid Malik <92035202+JamshaidMallik@users.noreply.github.com>
2023-06-19 16:41:30 +0500

    COMMIT CHECK

--- lib/screens/user_screens/commercial_area_screen.dart
+++ lib/screens/user_screens/commercial_area_screen.dart
@@ -18,6 +18,7 @@ class CommercialAreaScreen extends StatelessWidget {
       builder: (c) {
         return Scaffold(
           resizeToAvoidBottomInset: false,
+          
 
           appBar: AppBar(
             title: const Text(
